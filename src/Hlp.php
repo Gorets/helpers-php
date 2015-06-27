3ffd19a7a5042b5ac44fa3ba9bd1f77815b7642b
Vladimir Gorobets <vladimir.gorobets@gmail.com>
2015-06-27 10:03:56 +0300

    dbg add

--- src/Hlp.php
+++ src/Hlp.php
@@ -9,13 +9,32 @@
 namespace gorets\Addons;
 
 
-class HLP{
+class HLP
+{
+    private static $dbg_value = 1;
 
-    public static function pre($s){
+    /**
+     * @param $s
+     */
+    public static function pre($s)
+    {
+        if (self::dbg()) {
 
-        echo "<pre>";
-        print_r($s);
-        echo "</pre>";
+            echo "<pre>";
+            print_r($s);
+            echo "</pre>";
+        }
     }
 
+    /*
+    * функция для определения состояния отладки сайта
+    */
+    private static function dbg()
+    {
+        if (
+            ((isset($_COOKIE['dbg']) and $_COOKIE['dbg'] == self::$dbg_value) || (isset($_GET['dbg']) and $_GET['dbg'] == self::$dbg_value))
+            and (strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')
+        ) return true;
+        else return false;
+    }
 }
